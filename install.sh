ec6a6b9f5a9f2c56c0aaa7e4062569234de6bc8e
Arievies <ari.ve08@gmail.com>
2024-02-13 22:54:51 -0800

    Se modifico comando de intalacion de PHP

--- install.sh
+++ install.sh
@@ -7,21 +7,16 @@ sudo apt update -y && apt upgrade -y
 
 ##INSTALANDO APACHE
 echo  "Instalando Apache!!"
-
 sudo apt install apache2 -y
 
 
 echo "CAMBIANDO USUARIO A DIRECTORIO WIIIII"
-
 ##CAMBIANDO USUARIO A DIRECTORIO DE SERVIDOR
 sudo chown www-data:www-data -R /var/www/html
 
-
 ##CAMBIAR PERMISOS A DIRECTORIO  DEL SERVIDOR
 echo "CAMBIANDO PERMISOS A LA CARPETA WIIIII"
-
 sudo chmod 775 -R /var/www/html
-
 
 echo "Instalando PHP!!"
 
@@ -32,16 +27,13 @@ echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /et
 
 sudo apt update -y
 
-##INSTALANDO LIBRERIAS PHP7.4
+##INSTALANDO LIBRERIAS PHP-8.2
 #sudo apt-get install php8.2 php8.2-mbstring php8.2-mysqli php8.2-xml php8.2-curl php8.2-cli -y
-sudo apt install php8.2 php8.2-{bcmath,fpm,xml,mysql,zip,intl,ldap,gd,cli,bz2,curl,mbstring,pgsql,opcache,soap,cgiapcu,cgi} -y
+
+sudo apt install php8.2 php8.2-bcmath php8.2-fpm php8.2-mysql php8.2-xml php8.2-zip php8.2-intl php8.2-gd php8.2-cli php8.2-bz2 php8.2-curl php8.2-mbstring php8.2-pgsql php8.2-opcache php8.2-soap php8.2-cgi -y
 
 ##INSTALACION LIBRERIA libapache2
 sudo apt install libapache2-mod-php libapache2-mod-php8.2 -y
-
-##HABILITA  LA VERSION DE PHP QUE USARA APACHE
-sudo a2enmod php8.2
-
 
 ##INSTALANDO GIT,CURL
 sudo apt-get install curl git -y 
@@ -54,11 +46,8 @@ echo '<?php phpinfo(); ?>' | sudo tee /var/www/html/info.php
 
 ##INSTALACION MARIADB
 echo "INSTALACION MARIA DB"
-
 sudo apt install mariadb-server mariadb-client -y
-
 sudo mysql_secure_installation
-
 
 #GRANT ALL ON example_database.* TO 'root'@'localhost' IDENTIFIED BY 'fipaterm' WITH GRANT OPTION;
 #FLUSH PRIVILEGES;
@@ -76,7 +65,7 @@ gpg --verify phpMyAdmin-latest-all-languages.tar.gz.asc
 sudo mkdir /var/www/html/phpMyAdmin
 
 sudo tar xvf phpMyAdmin-latest-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpMyAdmin
-sudo cp /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/phpmyadmin/config.inc.php
+sudo cp /var/www/html/phpMyAdmin/config.sample.inc.php /var/www/html/phpMyAdmin/config.inc.php
 sudo systemctl restart apache2
 
 
@@ -94,6 +83,9 @@ sudo apt install nodejs npm -y
 node -v 
 
 echo "Reiniciando Servicios!!"
+
+##HABILITA  LA VERSION DE PHP QUE USARA APACHE
+sudo a2enmod php8.2
 
 ##REINICIANDO SERVICIOS APACHE2
 sudo systemctl reload apache2
